5edc1601fb9a2a82a25068e757726c61791381ec
Hatsune07 <frostleaf363@gmail.com>
2024-01-09 14:19:35 +0000

    temp

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP PREEMPT Tue Jan 9 14:05:28 UTC 2024"
+#define LINUX_COMPILE_BY "Frostleaf"
+#define LINUX_COMPILE_HOST "Darkness"
+#define LINUX_COMPILER "clang version 10.0.0-4ubuntu1 "
