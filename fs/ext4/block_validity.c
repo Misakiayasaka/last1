47a81952f8782643b34ff224eae25b55f32b617f
Hatsune07 <frostleaf363@gmail.com>
2024-01-10 00:24:02 +0000

    ext4 val

--- fs/ext4/block_validity.c
+++ fs/ext4/block_validity.c
@@ -136,50 +136,6 @@ static void debug_print_tree(struct ext4_sb_info *sbi)
 	printk(KERN_CONT "\n");
 }
 
-static int ext4_protect_reserved_inode(struct super_block *sb, u32 ino)
-{
-	struct inode *inode;
-	struct ext4_sb_info *sbi = EXT4_SB(sb);
-	struct ext4_map_blocks map;
-	u32 i = 0, num;
-	int err = 0, n;
-
-	if ((ino < EXT4_ROOT_INO) ||
-	    (ino > le32_to_cpu(sbi->s_es->s_inodes_count)))
-		return -EINVAL;
-	inode = ext4_iget(sb, ino, EXT4_IGET_SPECIAL);
-	if (IS_ERR(inode))
-		return PTR_ERR(inode);
-	num = (inode->i_size + sb->s_blocksize - 1) >> sb->s_blocksize_bits;
-	while (i < num) {
-		cond_resched();
-		map.m_lblk = i;
-		map.m_len = num - i;
-		n = ext4_map_blocks(NULL, inode, &map, 0);
-		if (n < 0) {
-			err = n;
-			break;
-		}
-		if (n == 0) {
-			i++;
-		} else {
-			if (!ext4_data_block_valid(sbi, map.m_pblk, n)) {
-				ext4_error(sb, "blocks %llu-%llu from inode %u "
-					   "overlap system zone", map.m_pblk,
-					   map.m_pblk + map.m_len - 1, ino);
-				err = -EFSCORRUPTED;
-				break;
-			}
-			err = add_system_zone(sbi, map.m_pblk, n);
-			if (err < 0)
-				break;
-			i += n;
-		}
-	}
-	iput(inode);
-	return err;
-}
-
 int ext4_setup_system_zone(struct super_block *sb)
 {
 	ext4_group_t ngroups = ext4_get_groups_count(sb);
